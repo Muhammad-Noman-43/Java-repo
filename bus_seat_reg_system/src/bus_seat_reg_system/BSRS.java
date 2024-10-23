75aa7a5626311c3075243204e8cbd9a6091d7f5e
Muhammad Noman <muhammadnoman.cs@gmail.com>
2024-10-23 13:22:47 +0500

    These are some of the projects that i have created in my OOP courrse at CUI Dhamtour Campus

---
+++ bus_seat_reg_system/src/bus_seat_reg_system/BSRS.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bus_seat_reg_system;
+
+/**
+ *
+ * @author Muhammad Noman
+ */
+public class BSRS {
+    int bus_no;
+}
