0c5ebf74a4c8f050fea504154c743bbc96916551
Muhammad Noman <muhammadnoman.cs@gmail.com>
2024-10-24 23:13:14 +0500

    This was commited in netbeans ide

---
+++ Library_management_system/src/library_management_system/Member.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library_management_system;
+
+/**
+ *
+ * @author Muhammad Noman
+ */
+public class Member {
+    String name;
+    int memberID;
+    int limit;
+    
+}
